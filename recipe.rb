15d3c75441861d389e59dbb24d9686360acefa5c
John Hawthorn <john.hawthorn@gmail.com>
2016-05-18 20:53:58 -0700

    Initial commit

---
+++ recipe.rb
@@ -0,0 +1,22 @@
+class Fzy < FPM::Cookery::Recipe
+  homepage 'http://github.com/jhawthorn/fzy'
+  source   'https://github.com/jhawthorn/fzy/archive/0.3.tar.gz'
+  md5      '2a0c15d51f68d6f5bd05e8129b651264'
+
+  name     'fzy'
+  version  '0.3'
+  revision '1'
+
+  maintainer 'John Hawthorn <john.hawthorn@gmail.com>'
+  license 'MIT'
+
+  description 'A better fuzzy finder.'
+
+  def build
+    make
+  end
+
+  def install
+    make :install, 'DESTDIR' => destdir, 'PREFIX' => '/usr'
+  end
+end
