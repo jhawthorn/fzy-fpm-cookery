bb6b6e6f2debb85a0d5839f38b7adeb2169e68ab
John Hawthorn <john.hawthorn@gmail.com>
2016-07-26 19:02:02 -0700

    Version 0.6

--- recipe.rb
+++ recipe.rb
@@ -1,10 +1,10 @@
 class Fzy < FPM::Cookery::Recipe
   homepage 'http://github.com/jhawthorn/fzy'
-  source   'https://github.com/jhawthorn/fzy/archive/0.5.tar.gz'
-  md5      '209cd9dce5d3a371bc8f3bea4c0bdb1b'
+  source   'https://github.com/jhawthorn/fzy/archive/0.6.tar.gz'
+  md5      '8ed6973d03e484587c6bd2081314b659'
 
   name     'fzy'
-  version  '0.5'
+  version  '0.6'
   revision '1'
 
   maintainer 'John Hawthorn <john.hawthorn@gmail.com>'
